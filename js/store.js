d12ffdb5fa9eaf726ad72493c720150c483aa66f
OTeeEnabor <tobienabor@gmail.com>
2019-07-28 00:52:03 +0200

    setup the spacing between eah of the navigation links

--- js/store.js
+++ js/store.js
@@ -1,6 +1,6 @@
 // Query the DOM for all h2 and assign it to a variable 
 let h2styles = document.querySelectorAll('h2');
-//add a a class to all h2 elements in the web page to change their css styling.
+//Add  a class to all h2 elements in the web page to change their css styling.
 h2styles.forEach(h2stlye => {
 
     h2stlye.classList.add('heading-2')

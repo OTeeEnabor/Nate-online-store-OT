0342acb4627ec88e147f5950c704579f86e2aea6
OTeeEnabor <tobienabor@gmail.com>
2019-08-07 14:13:19 +0200

    incoperated changes to the scroll to top button suggested from coach session

--- js/shop.js
+++ js/shop.js
@@ -53,77 +53,209 @@ jean[2] = jean_4;
 jean[3]= jean_5;
 
 for (i=0; i < jean.length ;i++){
-
     jean[i].createHtml();
 }
 
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded',load)
+} else{
+    load()
+}
 
-// add items to the cart function
-(function(){
-const cartButton = document.querySelectorAll('.shop-button');
-cartButton.forEach (function(button){
-    button.addEventListener('click', function(event) {
-        if (event.target.parentElement.classList.contains("card-body")){
+ function load() {
+    let removeButton = document.getElementsByClassName('cart-button')
+ 
+    for (var i = 0; i < removeButton.length; i++) {
+        var button = remoceCartItem[i]
+        button.addEventListener('click',removeCartItem)
+    }
 
-            let fullPath= event.target.parentElement.previousElementSibling.src;//get the path of the image file associated with this button
-            let pos = fullPath.indexOf("images");
-            let partPath = fullPath.slice(pos);
-            console.log(partPath);
-            //Create cart item object
-            const item = {};
-            item.img = partPath; //cart item image given a path
-            let name = event.target.previousElementSibling.previousElementSibling.previousElementSibling.innerText;
-            item.name = name;
-            let price = parseFloat(event.target.previousElementSibling.previousElementSibling.innerText.replace('R',''));
-            item.price=price;
+    var quantityInputs = document.getElementsByClassName('cart-quantity');
+    for (var i = 0; i < quantityInputs.length; i++) {
+        var input = quantityInput[i]
+        input.addEventListener('change',quantityChanged)
 
-            const cartItem = document.createElement("div");
-            cartItem.classList.add('row','cart-item-row')
-            cartItem.innerHTML = `
+    }
+
+    let addTocartButton = document.getElementsByClassName('shop-button')
+    for (var i = 0; i < addTocartButton.length; i++) {
+        var button = addTocartButton[i]
+        button.addEventListener('click',addToCart)
+
+    }
+
+
+ }
+
+ function removeCartItem(event) {
+     var buttonClicked = event.target
+     buttonClicked.parentElement.parentElement.parentElement.remove()
+     updateCartTotal()
+ }
+
+ function quantityChanged(event) {
+     var input = event.target
+     if (isNaN(input.value) || input.value <=0) {
+         
+        input.value = 1
+     }
+     updateCartTotal()
+        
+
+ }
+
+ function addToCart(event){
+     var button = event.target
+     var shopItem = button.parentElement.classList.contains("card-body")
+     let fullPath= event.target.parentElement.previousElementSibling.src;
+    //  get the path of the image file associated with this button
+                 let pos = fullPath.indexOf("images");
+                 let partPath = fullPath.slice(pos);
+                 //Create cart item object
+                 const item = {};
+                 item.img = partPath; //cart item image given a path
+                 let name = event.target.previousElementSibling.previousElementSibling.previousElementSibling.innerText;
+                 item.name = name;
+                 let price = parseFloat(event.target.previousElementSibling.previousElementSibling.innerText.replace('R',''));
+                 item.price=price;
+     
+                 const cartItem = document.createElement("div");
+                 cartItem.classList.add('row','cart-item-row')
+                 cartItem.innerHTML = `
+                  
+                     <div class="col-4 cart-item">
+                         <img class="cart-item-image"src="${item.img}">
+                         <span class="cart-text cart-item-title">${item.name}</span>
+                     </div>
+                     <div class=" col-4 cart-price">
+                         <span class=" cart-text cart-item-price">R${item.price}</span>
+                     </div>
+                     <div class="col-4 cart-quantity">
+                         <div class="quantity-container">
+                                 <input class="cart-quantity" type="number" value="1">
+                                 <button class="btn shop-button cart-button cart-text" type="button">Remove Item</button>
+                         </div>
+                         
+                     </div>
+                 </div>
+                 `;
+                 //This code selects the names of items in the cart.
+                 let cartItemNames = document.getElementsByClassName('cart-item-title');
+                 //Loop through all the item names in the cart
+                  for (let i = 0; i < cartItemNames.length; i++) {
+                     if (cartItemNames[i].innerText == name){
+                         alert(`${item.name} has already been added to the cart, increase your quantity.`)
+                         return
+                     }
+                 }
+                 alert(`${item.name} has been added to the cart`)
+                 const cartSection = document.getElementById('cartSection');
+                 const cartTotal = document.getElementById('cartTotal');
+                 cartSection.insertBefore(cartItem,cartTotal);
+
+ }
+// // add items to the cart function
+// (function(){
+// const cartButton = document.querySelectorAll('.shop-button');
+// cartButton.forEach (function(button){
+//     button.addEventListener('click', function(event) {
+//         if (event.target.parentElement.classList.contains("card-body")){
+//             let fullPath= event.target.parentElement.previousElementSibling.src;//get the path of the image file associated with this button
+//             let pos = fullPath.indexOf("images");
+//             let partPath = fullPath.slice(pos);
+//             //Create cart item object
+//             const item = {};
+//             item.img = partPath; //cart item image given a path
+//             let name = event.target.previousElementSibling.previousElementSibling.previousElementSibling.innerText;
+//             item.name = name;
+//             let price = parseFloat(event.target.previousElementSibling.previousElementSibling.innerText.replace('R',''));
+//             item.price=price;
+
+//             const cartItem = document.createElement("div");
+//             cartItem.classList.add('row','cart-item-row')
+//             cartItem.innerHTML = `
              
-                <div class="col-4 cart-item">
-                    <img class="cart-item-image"src="${item.img}">
-                    <span class="cart-text cart-item-title">${item.name}</span>
-                </div>
-                <div class=" col-4 cart-price">
-                    <span class=" cart-text cart-item-price">R${item.price}</span>
-                </div>
-                <div class="col-4 cart-quantity">
-                    <div class="quantity-container">
-                            <input class="cart-quantity" type="number" value="1">
-                            <button class="btn shop-button cart-button cart-text" type="button">Remove Item</button>
-                    </div>
+//                 <div class="col-4 cart-item">
+//                     <img class="cart-item-image"src="${item.img}">
+//                     <span class="cart-text cart-item-title">${item.name}</span>
+//                 </div>
+//                 <div class=" col-4 cart-price">
+//                     <span class=" cart-text cart-item-price">R${item.price}</span>
+//                 </div>
+//                 <div class="col-4 cart-quantity">
+//                     <div class="quantity-container">
+//                             <input class="cart-quantity" type="number" value="1">
+//                             <button class="btn shop-button cart-button cart-text" type="button">Remove Item</button>
+//                     </div>
                     
-                </div>
-            </div>
-            `;
-            //Select the cart
+//                 </div>
+//             </div>
+//             `;
+//             //This code selects the names of items in the cart.
+//             let cartItemNames = document.getElementsByClassName('cart-item-title');
+//             //Loop through all the item names in the cart
+//              for (let i = 0; i < cartItemNames.length; i++) {
+//                 if (cartItemNames[i].innerText == name){
+//                     alert(`${item.name} has already been added to the cart, increase your quantity.`)
+//                     return
+//                 }
+//             }
+//             alert(`${item.name} has been added to the cart`)
+//             const cartSection = document.getElementById('cartSection');
+//             const cartTotal = document.getElementById('cartTotal');
+//             cartSection.insertBefore(cartItem,cartTotal);
+//             let removeCartButtons = document.getElementsByClassName('cart-button');
+// console.log(removeCartButtons)
+            
+//         };        
 
-            const cartSection = document.getElementById('cartSection');
-            console.log(cartSection)
-            const cartTotal = document.getElementById('cartTotal');
-            console.log(cartTotal)
-            cartSection.insertBefore(cartItem,cartTotal);
-            alert('Item has been added to cart:)')
-        };        
+//     });
 
-    });
+// });
+// })();
+// cartNotEmpty = document.getElementsByClassName('cart-button');
+// console.log(cartNotEmpty)
+// if (document.readyState == 'loading' & cartNotEmpty != null) {
+// document.addEventListener('DOMContentLoaded',loaded)
 
-});
+// } else {
+//     loaded()
+// }
 
-})();
+// function loaded(){
+//      let removeCart = document.getElementsByClassName('cart-button');
+//      console.log(removeCart)
+//     for (let i = 0; i < removeCart.length;i++) {
+//         let button = removeCart[i]
+//         button.addEventListener('click', function(event) {
+//             var buttonClicked = event.target
+//             console.log(buttonClicked.parentElement.parentElement.parentElement)
+//             buttonClicked.parentElement.parentElement.parentElement.remove()
+//         });
+//     }
 
-(function(){
-    var removeCartButtons = document.getElementsByClassName('cart-button')
-    for (var i = 0; i < removeCartButtons.length;i++) {
-        var button = removeCartButtons[i]
-        button.addEventListener('click', function(event){
-            var buttonClicked = event.target
-            buttonClicked.parentElement.parentElement.parentElement.remove()
-            updateCartTotal()
-        });
-    };
-}());
+// }
+
+// function removeCartItem(event) {
+
+//             var buttonClicked = event.target
+//             console.log(buttonClicked.parentElement.parentElement.parentElement)
+//             buttonClicked.parentElement.parentElement.parentElement.remove()
+// }
+// (function() {
+//     let removeCart = document.getElementsByClassName('cart-button');
+//      console.log(removeCart)
+//     for (let i = 0; i < removeCart.length;i++) {
+//         let button = removeCart[i]
+//         button.addEventListener('click', function(event) {
+//             var buttonClicked = event.target
+//             console.log(buttonClicked.parentElement.parentElement.parentElement)
+//             buttonClicked.parentElement.parentElement.parentElement.remove()
+//         });
+//     }
+// })();
+
+
 
 // (function quantityChange(event) {
 //     var input = event.target
@@ -134,14 +266,19 @@ cartButton.forEach (function(button){
 
 // })();
 
-function updateCartTotal() {
-var cartHolder = document.getElementsByClassName('cart-item')[0];
- var cart = cartHolder.getElementsByClassName('cart-item-row')
- for (var i = 0; i < cartRows.length; i++) {
-     var cartRow = cartRows[i];
-     var priceElem = cartRow.getElementsByClassName('cart-price')[0];
-     var quantityElem = cartRow.getElementsByClassName('cart-quantity')[0];
- }
- console.log(priceElem)
+// function updateCartTotal() {
+// var cartHolder = document.getElementsByClassName('cart-item')[0];
+//  var cart = cartHolder.getElementsByClassName('cart-item-row')
+//  for (var i = 0; i < cartRows.length; i++) {
+//      var cartRow = cartRows[i];
+//      var priceElem = cartRow.getElementsByClassName('cart-price')[0];
+//      var quantityElem = cartRow.getElementsByClassName('cart-quantity')[0];
+//  }
+ 
 
-}
+// }
+
+// function showTotal(){
+
+//     console.log(alert('sdsd'))
+// };

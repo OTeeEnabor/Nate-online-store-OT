b5534889602253277a242bc32ee5659a473f7889
OTeeEnabor <tobienabor@gmail.com>
2019-08-09 09:18:59 +0200

    Add to cart functional

--- js/shop.js
+++ js/shop.js
@@ -36,44 +36,44 @@ function Product(name, price,brand,path) {
     this.path = path;
     this.description = "This " + this.name + " is made by " + this.brand + ". Do best and get yours now.";
 }
-
 //Product Prototypes
-
+//The code below creates a prototype method for the Product object that gives a brief description of the object
 Product.prototype.description = function () {
     let snippet = "This " + this.name + " is made by " + this.brand + ". Do best and get yours now."
     return snippet;
 };
-
+//The code below creates a prototype method for the Product object that adds the object into the DOM of the shop page
 Product.prototype.createHtml = function() {
     const shop = document.getElementById('Row');
     const shopItem = document.createElement("div");
     shopItem.classList.add("col-3",'shop-product');
     shopItem.innerHTML = `
            <div class="card" style="width: 20rem;">
-                <img src="${this.path}" class="card-img-top" alt="...">
+                <img src="${this.path}" class="card-img-top" alt="${this.name}">
                 <div class="card-body">
                     <h5 class="card-title shop-item-title">${this.name}</h5>
                     <p class="card-text currency"><span class="currency">R </span>${this.price}</p>
                     <p class="card-text item-description">${this.description}</p>
-                    <button class="btn shop-button" type="button onclick=removeCartItem(event)"><i class='fas fa-shopping-cart'></i>Add to cart</button>
+                    <button class="btn shop-button" type="button" onclick="load()"><i class='fas fa-shopping-cart'></i>Add to cart</button>
                 </div>
            </div>`;
     
     shop.appendChild(shopItem);
 };
 
-
+//The code block below creates 4 jeans objects
 let jean_2 = new Product('Biker Jeans',550,'Nlu','/images/products/jeans/jeans_2.jpg');
 let  jean_3 = new Product('Skinny Jeans',500,'Yung','/images/products/jeans/jeans_3.jpg');
 let jean_4 = new Product('Skinny Blue',500,'Biker Blue','/images/products/jeans/jeans_4.jpg');
 let jean_5 = new Product('Dark Biker',550,'NlU','/images/products/jeans/jeans_1.jpg');
-
+//Create an array to store the 4 jeans objects
 const jean = new Array();
 jean[0] = jean_2;
 jean[1] = jean_3;
 jean[2] = jean_4;
 jean[3]= jean_5;
 
+//For loop to loop through the array and place each jean object into the shop page DOM
 for (i=0; i < jean.length ;i++){
     jean[i].createHtml();
 }
@@ -86,32 +86,32 @@ if (document.readyState == 'loading') {
 
  function load() {
 
-    let removeButton = document.getElementsByClassName('cart-button');
+    let removeButton = document.getElementsByClassName('cart-button')
     for (var i = 0; i < removeButton.length; i++) {
-        var button = remoceCartItem[i]
-        button.addEventListener('click',removeCartItem)
+        var button = removeButton[i];
+        button.addEventListener('click',removeCartItem);
     }
 
     var quantityInputs = document.getElementsByClassName('cart-quantity');
     for (var i = 0; i < quantityInputs.length; i++) {
-        var input = quantityInput[i]
-        input.addEventListener('change',quantityChanged)
+        var input = quantityInputs[i];
+        input.addEventListener('change',quantityChanged);
 
     }
 
-    let addTocartButton = document.getElementsByClassName('shop-button');
-    for (var i = 0; i < addTocartButton.length; i++) {
-        var button = addTocartButton[i]
-        button.addEventListener('click',addToCart)
-
+    let addToCartButton = document.getElementsByClassName('shop-button');
+    for (var i = 0; i < addToCartButton.length; i++) {
+        var button = addToCartButton[i];
+        button.addEventListener('click',addToCart);
     }
 
 
- }
+ };
 
  function removeCartItem(event) {
      var buttonClicked = event.target
-     buttonClicked.parentElement.parentElement.parentElement.remove()
+     console.log(buttonClicked);
+     buttonClicked.parentElement.parentElement.parentElement.remove();
      updateCartTotal()
  }
 
@@ -126,17 +126,43 @@ if (document.readyState == 'loading') {
 
  }
 
+ function updateCartTotal() {
+    //get the all rows in the cart
+    let cartItemRows  = document.getElementsByClassName("cart-item-row");
+    let total = 0;
+    for (let i = 0; i < cartItemRows.length;i++){
+       let cartItemRow = cartItemRows[i];
+      let priceItem = cartItemRow.getElementsByClassName('cart-item-price')[0];
+      let quantityItem = cartItemRow.getElementsByClassName('cart-quantity-input')[0];
+       let price= parseFloat(priceItem.innerText.replace('R',''));
+       let quantity = quantityItem.value;
+       //increment the total amount in the cart by the price and quantity of each item in the cart. 
+
+       total = total + (price * quantity)
+       console.log(price*quantity)
+    }
+    document.getElementsByClassName('cart-total-amount')[0].innerText = "Total Amount: R"+ total;
+    // let totalAmount = document.getElementsByClassName('cart-total-amount')[0];
+    // console.log(totalAmount)
+    // totalAmount.innerText = total;
+    // console.log(total)
+    // let total = document.getElementsByClassName('cart-item-price')[0];
+    // let totals = parseFloat(total.innerText.replace('R',''));
+    // console.log(totals)
+ }
+
  function addToCart(event){
      var button = event.target
-     var shopItem = button.parentElement.classList.contains("card-body")
+     var shopItem = button.parentElement.classList.contains("card-body");
+
      //Create cart item object
      const item = {};
      //  get the path of the image file associated with this button
      let fullPath= event.target.parentElement.previousElementSibling.src;
     let pos = fullPath.indexOf("images");
     let partPath = fullPath.slice(pos);
-
-    item.img = partPath; //cart item image given a path
+    item.img = partPath; 
+    //cart item image given a path
     //get the name of the shop item associated with the add to cart button clicked
     let name = event.target.previousElementSibling.previousElementSibling.previousElementSibling.innerText;
     //Assign the name of the shop item to as a property of the item object
@@ -147,9 +173,7 @@ if (document.readyState == 'loading') {
     item.price=price;
      //Create a div container to place this cart item object
     const cartItem = document.createElement("div");
-
     cartItem.classList.add('row','cart-item-row');
-
     cartItem.innerHTML = `
         <div class="col-4 cart-item">
             <img class="cart-item-image"src="${item.img}">
@@ -160,13 +184,15 @@ if (document.readyState == 'loading') {
         </div>
         <div class="col-4 cart-quantity">
             <div class="quantity-container">
-                    <input class="cart-quantity" type="number" value="1">
-                    <button class="btn shop-button cart-button cart-text" type="button">Remove Item</button>
+                    <input class="cart-quantity-input" type="number" value="1" onclick="quantityChanged(event)">
+                    <button class="btn shop-button cart-button cart-text" type="button" onclick="removeCartItem(event)">Remove Item</button>
             </div>
             
         </div>
     </div>
     `;
+
+
     //This code selects the names of items in the cart.
     let cartItemNames = document.getElementsByClassName('cart-item-title');
 
@@ -181,8 +207,76 @@ if (document.readyState == 'loading') {
     const cartSection = document.getElementById('cartSection');
     const cartTotal = document.getElementById('cartTotal');
     cartSection.insertBefore(cartItem,cartTotal);
+    updateCartTotal();
+ };
 
- }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // // add items to the cart function
 // (function(){
 // const cartButton = document.querySelectorAll('.shop-button');
